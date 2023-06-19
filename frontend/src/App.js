d09157b7f6f1430a3aea47a4ffc61c360a31f0b5
ANGELA <parkboyoungunnie@gmail.com>
2023-06-19 10:57:22 +0800

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Gonzales Guitars</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
